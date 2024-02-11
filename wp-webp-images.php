fd3479bbaac1ec2e98e460d2185df9cc8dee6cbe
Yurii Hryhoriev <y.hryhoriev.1994@gmail.com>
2024-02-11 13:21:20 +0100

    initial commit

---
+++ wp-webp-images.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Plugin Name: WP Webp Images
+ * Plugin URI: https://hryhoriev.website/wordpress/plugins/wp-webp-images
+ * Description: "WP Webp Images" is a WordPress plugin designed to automatically create WebP format copies of both newly uploaded and existing images. It ensures file size reduction without quality loss, significantly speeding up page loading and enhancing site performance. Easy to install and use, WP Webp Images is perfect for boosting the speed and efficiency of your WordPress site.
+ * Version: 1.0
+ * Author: Yurii Hryhoriev
+ * Author URI: https://hryhoriev.website
+ */
